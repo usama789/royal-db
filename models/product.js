c0ff28290de5bd28ee2919ae2114d2ce5a172834
usama789 <>
2020-11-30 22:22:24 +0500

    n

--- models/product.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var productSchema = mongoose.Schema({
-    name: String,
-    price: String,
-}); 
-
-const Product = mongoose.model("Product", productSchema);
-module.exports = Product;
